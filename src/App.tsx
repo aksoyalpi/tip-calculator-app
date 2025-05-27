a98885e3fbddc3eb34ec8456c2943600af55e7da
Alperen <aksoyalpi@gmail.com>
2025-05-27 21:09:19 +0200

    Finished styling of desktop version

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,8 @@ function App() {
     if(customTip !== undefined){
       await setTip(customTip);
     }
-    setTotal(money * tip!/100);
-    setTipAmount(money * tip!/100/people);
+    setTotal(Number((money * tip!/100).toFixed(2)));
+    setTipAmount(Number((money * tip!/100/people).toFixed(2)));
   }
 
   const handleTipClick = (tip: number) => {
@@ -49,58 +49,60 @@ function App() {
   return (
     <div className='grid bg-grey-200 place-content-center w-full h-dvh gap-10 justify-center content-center'>
       <h1 className='text-center text-grey-500 font-bold text-2xl'>S P L I<br/>T T E R</h1>
-      <div className='rounded-t-2xl p-5 h-full grid gap-5 bg-white'>
-        <div>
-          <h5 className='text-grey-200'>Bill</h5>
-          <div className='input-container'>
-            <i className="fa fa-dollar icon"></i>
-            <input className='form-input' value={money.toString()} type='number' 
-            onChange={(e) => {
-              console.log(Number(e.target.value));
-              setMoney(Number(e.target.value));
-            }}/>
-          </div>
-        </div>
-
-        <div>
-          <h5>Select Tip %</h5>
-          <div className='grid grid-cols-2 gap-4'>
-            <button className={'tip ' + (tip === 5 ? "bg-green-400 text-black" : "")} id='5' 
-            onClick={() => handleTipClick(5)}>5%</button>
-            <button className={'tip ' + (tip === 10 ? "bg-green-400 text-black" : "")} id='10' 
-            onClick={() => handleTipClick(10)}>10%</button>
-            <button className={'tip ' + (tip === 15 ? "bg-green-400 text-black" : "")} id='15' 
-            onClick={() => handleTipClick(15)}>15%</button>
-            <button className={'tip ' + (tip === 25 ? "bg-green-400 text-black" : "")} id='25' 
-            onClick={() => handleTipClick(25)}>25%</button>
-            <button className={'tip ' + (tip === 50 ? "bg-green-400 text-black" : "")} id='50' 
-            onClick={() => handleTipClick(50)}>50%</button>
-            <input placeholder='Custom' className='form-input' value={customTip === undefined ? "":customTip.toString()} onChange={(e) => handleCustom(Number(e.target.value))} type='number'/>
-          </div>
-        </div>
-
-        <div>
-          <h5>Number of People</h5>
-          <div className='input-container'>
-            <i className="fa fa-user icon"></i>
-            <input className='form-input' value={people} type='number' onChange={(e) => setPeople(Number(e.target.value))}/>
-          </div>
-        </div>
-
-        <div className='grid grid-cols-2 gap-5 bg-green-900 text-white rounded-2xl p-6 justify-around'>
+      <div className='grid h-full gap-5 mb-0 bg-white rounded-t-2xl p-5 md:grid-cols-2 md:m-10'>
+        <div className='grid gap-5'>
           <div>
+            <h5 className='text-grey-200'>Bill</h5>
+            <div className='input-container'>
+              <i className="fa fa-dollar icon"></i>
+              <input className='form-input' value={money.toString()} type='number' 
+              onChange={(e) => {
+                setMoney(Number(Number(e.target.value).toFixed(2)));
+              }}/>
+            </div>
+          </div>
+
+          <div>
+            <h5>Select Tip %</h5>
+            <div className='grid grid-cols-2 gap-4'>
+              <button className={'tip ' + (tip === 5 ? "bg-green-400 text-black" : "")} id='5' 
+              onClick={() => handleTipClick(5)}>5%</button>
+              <button className={'tip ' + (tip === 10 ? "bg-green-400 text-black" : "")} id='10' 
+              onClick={() => handleTipClick(10)}>10%</button>
+              <button className={'tip ' + (tip === 15 ? "bg-green-400 text-black" : "")} id='15' 
+              onClick={() => handleTipClick(15)}>15%</button>
+              <button className={'tip ' + (tip === 25 ? "bg-green-400 text-black" : "")} id='25' 
+              onClick={() => handleTipClick(25)}>25%</button>
+              <button className={'tip ' + (tip === 50 ? "bg-green-400 text-black" : "")} id='50' 
+              onClick={() => handleTipClick(50)}>50%</button>
+              <input placeholder='Custom' className='form-input' value={customTip === undefined ? "":customTip.toString()} onChange={(e) => handleCustom(Number(e.target.value))} type='number'/>
+            </div>
+          </div>
+
+          <div>
+            <h5>Number of People</h5>
+            <div className='input-container'>
+              <i className="fa fa-user icon"></i>
+              <input className='form-input' value={people}  type='number' onChange={(e) => setPeople(Number(Number(e.target.value).toFixed(0)))}/>
+            </div>
+          </div>
+        </div>
+
+        
+        <div className='grid grid-cols-2 gap-5 grid-rows-3 md:grid-rows-4 bg-green-900 text-white rounded-2xl p-6 h-full justify-center'>
+          <div className='align-top'>
             <p>Tip Amount</p>
             <p className='text-grey-200 text-sm'>/ person</p>
           </div>
-          <p className='text-end text-green-400 font-bold text-3xl'>${tipAmount}</p>
+          <p className='text-end text-green-400 font-bold text-6xl'>${tipAmount}</p>
 
-          <div>
+          <div className='align-top'>
             <p>Total</p>
             <p className='text-grey-200 text-sm'>/ person</p>
           </div>
-          <p className='text-end text-green-400 font-bold text-3xl'>${total}</p>
+          <p className='text-end text-green-400 font-bold text-6xl'>${total}</p>
 
-          <button className='w-full bg-green-400 hover:bg-green-200 rounded-lg text-lg font-bold text-green-900 col-span-2 p-3' 
+          <button className='w-full max-h-12 mb-0 row-start-4 self-end  bg-green-400 hover:bg-green-200 rounded-lg text-lg font-bold text-green-900 col-span-2 p-3' 
                   onClick={handleReset}
           >RESET</button>
         </div>
